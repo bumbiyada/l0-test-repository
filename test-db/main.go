315c2080ed1694aa8439bc760aed94867c6d3126
= <bumbiyada@gmail.com>
2022-01-06 19:19:28 +0300

    fix test-db

--- test-db/main.go
+++ test-db/main.go
@@ -11,17 +11,18 @@ import (
 //const conf = "host=localhost port=5432 user=postgres password=123 dbname=test sslmode=disable"
 
 
-const url = "postgres://postgres:123@localhost:5432/test?sslmode=disable"
+const url = "postgres://postgres:123@db:5432/test?sslmode=disable"
 var schema = `
-CREATE TABLE main (
+DROP TABLE main;
+CREATE TABLE IF NOT EXISTS main (
 	idx text,
-	text text
+	texty text
 );
 `
 
 type Mystruct struct {
-	Idx  string
-	Text string
+	Idx  string	`db:"idx"`
+	Text string	`db:"texty"`
 }
 
 func main() {
@@ -31,17 +32,17 @@ func main() {
 	// init db
 	db.MustExec(schema)
 	// add values
-	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, text) VALUES (%s, %s)", "1", "text1"))
-	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, text) VALUES (%s, %s)", "2", "text2"))
-	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, text) VALUES (%s, %s)", "3", "text3"))
-	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, text) VALUES (%s, %s)", "4", "aboba1"))
-	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, text) VALUES (%s, %s)", "5", "aboba2"))
-	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, text) VALUES (%s, %s)", "6", "aboba3"))
+	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, texty) VALUES (%s, %s)", "'1'", "'text1'"))
+	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, texty) VALUES (%s, %s)", "'2'", "'text2'"))
+	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, texty) VALUES (%s, %s)", "'3'", "'text3'"))
+	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, texty) VALUES (%s, %s)", "'4'", "'aboba1'"))
+	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, texty) VALUES (%s, %s)", "'5'", "'aboba2'"))
+	db.MustExec(fmt.Sprintf("INSERT INTO main (idx, texty) VALUES (%s, %s)", "'6'", "'aboba3'"))
 	// get values
 	array := []Mystruct{}
-
+	log.Println("All values are inserted now we`ll try to show them")
 	db.Select(&array, "SELECT * FROM main ORDER BY idx ASC")
-
+	log.Println(array)
 	for _, val := range array {
 		log.Println(val)
 	}
